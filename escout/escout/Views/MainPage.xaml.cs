c831533344b8ad62e15e960655ade672287ba072
Sandro Ferreira <sandroacferreira@gmail.com>
2020-02-16 22:58:02 +0000

    Add: UserSettingsPage

---
+++ escout/escout/Views/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using escout.Models;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace escout.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : MasterDetailPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            OptionsListView.ItemsSource = GetOptions();
+            Detail = new NavigationPage(new UserDetailsPage());
+        }
+
+        private void OptionsListView_OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var option = e.SelectedItem as Option;
+
+            switch (option.Name)
+            {
+                case "User Details":
+                    Detail = new NavigationPage(new UserDetailsPage())
+                    {
+                        //BarBackgroundColor = Color.FromHex("#262626")
+                    };
+                    break;
+            }
+            IsPresented = false;
+        }
+
+        private List<Option> GetOptions()
+        {
+            var option = new List<Option>
+            {
+                new Option{Name="User Details", ImageUrl=""}
+            };
+
+            return option;
+        }
+    }
+}
---
+++ escout/escout/Views/UserDetailsPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using escout.Helpers;
+using escout.Models;
+using Newtonsoft.Json;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace escout.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class UserDetailsPage : ContentPage
+    {
+        public UserDetailsPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            GetData();
+        }
+
+        private async void GetData()
+        {
+            var response = RestConnector.GetDataFromApi(RestConnector.GetUserInfo);
+            var user = JsonConvert.DeserializeObject<User>(await response);
+
+            BindingContext = user;
+        }
+
+        private void Button_OnClicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
