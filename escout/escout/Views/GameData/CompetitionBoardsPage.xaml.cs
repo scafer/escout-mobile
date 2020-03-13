c511de191e0a37777ad3e9e08094238c850e5395
Sandro Ferreira <sandroacferreira@gmail.com>
2020-03-13 20:47:57 +0000

    Add: GameData views (#9)
    * Add: GameData views
    
    * Add: iOS images
    
    * Fix: iOS padding
    
    Co-authored-by: Sandro Ferreira <sandroferreira@Sandros-Mac.local>

--- escout/escout/Views/GameData/CompetitionBoardsPage.xaml.cs
+++ escout/escout/Views/GameData/CompetitionBoardsPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿
+using escout.Models;
 using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -15,6 +16,11 @@ namespace escout.Views
         private void SearchExecuted(object sender, EventArgs e)
         {
             activityIndicator.IsRunning = true;
+        }
+        private void ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var competitionBoard = e.SelectedItem as CompetitionBoard;
+            Navigation.PushAsync(new CompetitionBoardDetailsPage(competitionBoard));
         }
     }
 }
