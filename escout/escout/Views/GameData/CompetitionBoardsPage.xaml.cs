8cad9c5829eadeadbbc56bad718074be35b9751d
Sandro Ferreira <sandroacferreira@gmail.com>
2020-03-14 21:56:08 +0000

    Add: Get GameData (#10)
    * Add: GameData views
    
    * Add: iOS images
    
    * Add: Get GameData
    
    * Fix: red build

--- escout/escout/Views/GameData/CompetitionBoardsPage.xaml.cs
+++ escout/escout/Views/GameData/CompetitionBoardsPage.xaml.cs
@@ -1,6 +1,10 @@
 ﻿
 using escout.Models;
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using escout.Helpers;
+using Newtonsoft.Json;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -13,14 +17,41 @@ namespace escout.Views
         {
             InitializeComponent();
         }
-        private void SearchExecuted(object sender, EventArgs e)
+
+        private async void SearchExecuted(object sender, EventArgs e)
         {
             activityIndicator.IsRunning = true;
+            if (string.IsNullOrEmpty(key.Text))
+            {
+                if (await DisplayAlert("Info", "Load all data?", "Yes", "Cancel"))
+                    listView.ItemsSource = await GetCompetitionBoards("");
+            }
+            else
+            {
+                listView.ItemsSource =
+                    await GetCompetitionBoards(new SearchQuery(filter.Items[filter.SelectedIndex], "LIKE", key.Text)
+                        .ToString());
+            }
+
+            activityIndicator.IsRunning = false;
         }
+
         private void ItemSelected(object sender, SelectedItemChangedEventArgs e)
         {
             var competitionBoard = e.SelectedItem as CompetitionBoard;
             Navigation.PushAsync(new CompetitionBoardDetailsPage(competitionBoard));
+        }
+
+        private async Task<List<CompetitionBoard>> GetCompetitionBoards(string query)
+        {
+            List<CompetitionBoard> competitionBoards = new List<CompetitionBoard>();
+            var response = await RestConnector.GetObjectAsync(RestConnector.CompetitionBoard + query);
+            if (!string.IsNullOrEmpty(response))
+            {
+                competitionBoards = JsonConvert.DeserializeObject<List<CompetitionBoard>>(response);
+            }
+
+            return competitionBoards;
         }
     }
 }
--- escout/escout/Views/GameData/GamesPage.xaml.cs
+++ escout/escout/Views/GameData/GamesPage.xaml.cs
@@ -1,6 +1,10 @@
 ﻿
 using escout.Models;
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using escout.Helpers;
+using Newtonsoft.Json;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -14,14 +18,35 @@ namespace escout.Views
             InitializeComponent();
         }
 
-        private void SearchExecuted(object sender, EventArgs e)
+        private async void SearchExecuted(object sender, EventArgs e)
         {
             activityIndicator.IsRunning = true;
+            if (string.IsNullOrEmpty(key.Text))
+            {
+                if (await DisplayAlert("Info", "Load all data?", "Yes", "Cancel"))
+                    listView.ItemsSource = await GetGames("");
+            }
+            else
+            {
+                listView.ItemsSource = await GetGames(new SearchQuery(filter.Items[filter.SelectedIndex], "LIKE", key.Text).ToString());
+            }
+            activityIndicator.IsRunning = false;
         }
         private void ItemSelected(object sender, SelectedItemChangedEventArgs e)
         {
             var game = e.SelectedItem as Game;
             Navigation.PushAsync(new GameDetailsPage(game));
+        }
+
+        private async Task<List<Game>> GetGames(string query)
+        {
+            List<Game> games = new List<Game>();
+            var response = await RestConnector.GetObjectAsync(RestConnector.Games + query);
+            if (!string.IsNullOrEmpty(response))
+            {
+                games = JsonConvert.DeserializeObject<List<Game>>(response);
+            }
+            return games;
         }
     }
 }
