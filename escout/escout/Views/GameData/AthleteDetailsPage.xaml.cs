15659025fc8c14d06def4e95b81827100e6572a7
Sandro Ferreira <sandro_ferreira@outlook.pt>
2020-03-19 16:29:26 +0000

    Add: Rounded Images

--- escout/escout/Views/GameData/AthleteDetailsPage.xaml.cs
+++ escout/escout/Views/GameData/AthleteDetailsPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using escout.Models;
+﻿using System;
+using escout.Helpers;
+using escout.Models;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -11,6 +13,18 @@ namespace escout.Views
         {
             InitializeComponent();
             BindingContext = athlete;
+
+            if (true)
+                LoadImage(1);
+            else
+                Img.Source = "user_image.png";
+        }
+
+        private async void LoadImage(int imageId)
+        {
+            var img = await Utils.GetImage(imageId);
+            if (!String.IsNullOrEmpty(img.ImageUrl))
+                Img.Source = img.ImageUrl;
         }
     }
 }
