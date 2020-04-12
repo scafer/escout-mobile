89769bea0217ddfed5e871e6ab9087eab94d6f4d
Sandro Ferreira <sandroacferreira@gmail.com>
2020-04-12 04:02:29 +0100

    Change app version

--- escout/escout/App.xaml.cs
+++ escout/escout/App.xaml.cs
@@ -58,7 +58,7 @@ namespace escout
 
         public string Version
         {
-            get => Properties.ContainsKey(VersionKey) ? Properties[VersionKey].ToString() : "Version 0.5.5";
+            get => Properties.ContainsKey(VersionKey) ? Properties[VersionKey].ToString() : "Version 0.5.7";
             set => Properties[VersionKey] = value;
         }
 
