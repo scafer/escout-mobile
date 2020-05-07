862c596d7a7c1b175c00431f1ae43b08f8b6dd8b
Sandro Ferreira <sandroacferreira@gmail.com>
2020-05-07 15:40:52 +0100

    Refactor: mvvm pattern (#22)
    * Refactor: mvvm pattern
    
    * Update: nuggets
    
    * Update: Xamarin.Forms Nugget

--- escout/escout/Views/GameData/WatchingListPage.xaml.cs
+++ escout/escout/Views/GameData/WatchingListPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using escout.Helpers;
 using escout.Models.Db;
+using escout.ViewModels;
 using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -9,43 +10,30 @@ namespace escout.Views.GameData
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class WatchingListPage : ContentPage
     {
-        public WatchingListPage() => InitializeComponent();
+        public WatchingListPage()
+        {
+            InitializeComponent();
+        }
 
         protected override void OnAppearing()
         {
             base.OnAppearing();
-            LoadData();
+            BindingContext = new WatchingListViewModel(Navigation);
         }
 
-        private void GamesList_OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        private void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
         {
             var game = e.SelectedItem as DbGame;
             Navigation.PushAsync(new GameDetailsPage(game));
         }
 
-        private async void RemoveItem_OnClicked(object sender, EventArgs e)
+        private async void OnRemoveClicked(object sender, EventArgs e)
         {
             var menuItem = sender as MenuItem;
             var game = menuItem.CommandParameter as DbGame;
 
             var db = new LocalDb();
             await db.RemoveGameData(game.DataExt);
-            LoadData();
-        }
-
-        private async void LoadData()
-        {
-            var db = new LocalDb();
-            PendingGamesList.ItemsSource = await db.GetDbGame(0);
-            ActiveGamesList.ItemsSource = await db.GetDbGame(1);
-            FinishedGamesList.ItemsSource = await db.GetDbGame(2);
-        }
-
-        private async void MenuItem_OnClicked(object sender, EventArgs e)
-        {
-            var menuItem = sender as MenuItem;
-            var game = menuItem.CommandParameter as DbGame;
-            await Navigation.PushAsync(new GameDetailsPage(game));
         }
     }
 }
